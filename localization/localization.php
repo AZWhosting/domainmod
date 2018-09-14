821ca0a4b52226a239fa54c4702fb26f1760e185
AZWhosting <git@azurwebhosting.com>
2018-09-14 10:57:28 +0200

    Include Gettext

---
+++ localization/localization.php
@@ -0,0 +1,37 @@
+<?php
+//error_reporting(E_ALL | E_STRICT);
+if (session_id() == '') { // Si la session n'existe pas
+    session_start(); // On crée la session
+}
+// define constants
+define('PROJECT_DIR', DIR_ROOT);
+define('LOCALE_DIR', PROJECT_DIR .'/lang');
+//require_once(LOCALE_DIR .'/ok.php');
+define('DEFAULT_LOCALE', 'en_US');
+require_once('lib/gettext/gettext.inc');
+
+/**/
+ 
+$supported_locales = array('en_US', 'fr_FR');
+$encoding = 'UTF-8';
+//$locale = isset($_POST['lang']) ? $_POST['lang'] : (!empty($_GET['lang']) ? $_GET['lang']: (!empty($_SESSION['lang']) ? $_SESSION['lang']: DEFAULT_LOCALE));
+$locale = (isset($_POST['lang']))? $_POST['lang'] :(!empty($_SESSION['lang']) ? $_SESSION['lang']: DEFAULT_LOCALE);
+//On assigne cette variable de langue en SESSION
+$_SESSION['lang'] = $locale;
+
+/// gettext setup
+T_setlocale(LC_MESSAGES, $locale);
+// Set the text domain as 'messages'
+$domain = 'trad';
+T_bindtextdomain($domain, LOCALE_DIR);
+T_bind_textdomain_codeset($domain, $encoding);
+T_textdomain($domain);
+
+?>
+<?php
+/*echo"</br>";
+echo"</br>";
+echo __("Users");
+echo"</br>";
+var_dump($_SESSION['lang']);*/
+?>
